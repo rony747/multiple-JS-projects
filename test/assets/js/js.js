ffd1c95344f71ec6f54e3bc89f059226c3c793cd
rony from laptop <touhid_rony@yahoo.com>
2021-07-20 04:21:10 +0600

    Code for BMI

--- test/assets/js/js.js
+++ test/assets/js/js.js
@@ -1,1 +1,16 @@
-document.write("test");
+const hFeet = document.querySelector("#hFeet");
+const hInch = document.querySelector("#hInch");
+const wKg = document.querySelector("#wKg");
+const submit = document.querySelector("#submit");
+const result = document.querySelector("#result_area");
+
+submit.addEventListener("click", bmiFunc);
+
+function bmiFunc(event) {
+  event.preventDefault();
+  const height = hFeet.value * 0.3048 + hInch.value * 0.0254;
+  const weight = wKg.value;
+  const final = weight / Math.pow(height, 2);
+
+  result.innerHTML = final.toFixed(2);
+}
