6f896361856b3ca30b759a89a0db517c71fcd543
rony from laptop <touhid_rony@yahoo.com>
2021-07-20 04:38:31 +0600

    color code added

--- test/assets/js/js.js
+++ test/assets/js/js.js
@@ -3,6 +3,7 @@ const hInch = document.querySelector("#hInch");
 const wKg = document.querySelector("#wKg");
 const submit = document.querySelector("#submit");
 const result = document.querySelector("#result_area");
+const wStatus = document.querySelector("#wst");
 
 submit.addEventListener("click", bmiFunc);
 
@@ -10,7 +11,21 @@ function bmiFunc(event) {
   event.preventDefault();
   const height = hFeet.value * 0.3048 + hInch.value * 0.0254;
   const weight = wKg.value;
-  const final = weight / Math.pow(height, 2);
+  const calculation = weight / Math.pow(height, 2);
+  const final = calculation.toFixed(2);
 
-  result.innerHTML = final.toFixed(2);
+  result.innerHTML = final;
+
+  if (final < 16) {
+    wStatus.innerHTML = "Severe Thinness";
+    document.querySelector(".clm_thin .class1").classList.add("active");
+  }
+  if (final >= 16 && final < 17) {
+    wStatus.innerHTML = "Moderate Thinness";
+    document.querySelector(".clm_thin .class2").classList.add("active");
+  }
+  if (final >= 17 && final < 18.5) {
+    wStatus.innerHTML = "Mild Thinness";
+    document.querySelector(".clm_thin .class3").classList.add("active");
+  }
 }
