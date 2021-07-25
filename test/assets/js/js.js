222b8f50a590d6dfb8886f12a3d71d0fc18bae3a
rony from laptop <touhid_rony@yahoo.com>
2021-07-26 03:55:17 +0600

    color game

--- test/assets/js/js.js
+++ test/assets/js/js.js
@@ -1,48 +1,94 @@
-const hFeet = document.querySelector("#hFeet");
-const hInch = document.querySelector("#hInch");
-const wKg = document.querySelector("#wKg");
-const submit = document.querySelector("#submit");
-const result = document.querySelector("#result_area");
-const wStatus = document.querySelector("#wst");
+const squares = document.querySelectorAll(".square");
+const colorDisplay = document.getElementById("colorDisplay");
+const messageDisplay = document.querySelector("#message");
+const h1 = document.querySelector("h1");
+const resetButton = document.querySelector("#reset");
+const modeButtons = document.querySelectorAll(".mode");
+let colors = [];
+let pickedColor;
+let totalSquare = 6;
 
-submit.addEventListener("click", bmiFunc);
+init();
+function init() {
+  setUpSquares();
+  reset();
+}
 
-function bmiFunc(event) {
-  event.preventDefault();
-
-  const height = hFeet.value * 0.3048 + hInch.value * 0.0254;
-  const weight = wKg.value;
-  const calculation = weight / Math.pow(height, 2);
-  const final = calculation.toFixed(2);
-  result.innerHTML = final;
-
-  document.querySelectorAll(".bmi_scale .column").forEach((el) => {
-    el.classList.remove("active");
-  });
-
-  if (final < 16) {
-    wStatus.innerHTML = "Severe Thinness";
-    document.querySelector(".thin1").classList.add("active");
-  } else if (final >= 16 && final < 17) {
-    wStatus.innerHTML = "Moderate Thinness";
-    document.querySelector(".thin2").classList.add("active");
-  } else if (final >= 17 && final < 18.5) {
-    wStatus.innerHTML = "Mild Thinness";
-    document.querySelector(".thin3").classList.add("active");
-  } else if (final > 18.5 && final <= 25) {
-    wStatus.innerHTML = "Normal";
-    document.querySelector(".norm1").classList.add("active");
-  } else if (final > 25 && final <= 30) {
-    wStatus.innerHTML = "Overweight";
-    document.querySelector(".ow1").classList.add("active");
-  } else if (final > 30 && final <= 35) {
-    wStatus.innerHTML = "Obese Class I";
-    document.querySelector(".obs1").classList.add("active");
-  } else if (final >= 35 && final <= 40) {
-    wStatus.innerHTML = "Mild Thinness";
-    document.querySelector(".obs2").classList.add("active");
-  } else if (final > 40) {
-    wStatus.innerHTML = "Mild Thinness";
-    document.querySelector(".obs3").classList.add("active");
+function setUpSquares() {
+  for (let i = 0; i < squares.length; i++) {
+    //add initial colors
+    //squares[i].style.background = colors[i];
+    //add click listeners
+    squares[i].addEventListener("click", function () {
+      //grab color of clicked color
+      let clickedColor = this.style.background;
+      //compare color
+      if (clickedColor === pickedColor) {
+        changeColors(clickedColor);
+        messageDisplay.textContent = "Correct";
+        resetButton.textContent = "Play again?";
+        h1.style.background = clickedColor;
+      } else {
+        this.style.background = "white";
+        messageDisplay.textContent = "Try Again";
+      }
+    });
   }
 }
+
+function reset() {
+  //generate all new color
+  colors = randomColor(totalSquare);
+  //pick new random color from array
+  pickedColor = pickColor();
+  //change color display to match picked color
+  colorDisplay.textContent = pickedColor;
+  resetButton.textContent = "New colors";
+  messageDisplay.textContent = "";
+  //change color of squares
+  for (let i = 0; i < squares.length; i++) {
+    if (colors[i]) {
+      squares[i].style.display = "block";
+      squares[i].style.background = colors[i];
+    } else {
+      squares[i].style.display = "none";
+    }
+  }
+  h1.style.background = "#cb7575";
+}
+
+resetButton.addEventListener("click", function () {
+  reset();
+});
+
+colorDisplay.textContent = pickedColor;
+
+function changeColors(color) {
+  //loop all squares
+  for (var i = 0; i < squares.length; i++) {
+    //change each color to match given color
+    squares[i].style.background = color;
+  }
+}
+function pickColor() {
+  let random = Math.floor(Math.random() * colors.length);
+  return colors[random];
+}
+function randomColor(num) {
+  //make array
+  var arr = [];
+  //repeat num times
+  for (var i = 0; i < num; i++) {
+    arr.push(randomColor());
+    //get random color and push into array
+  }
+  //return that array
+  return arr;
+}
+function rgbColor() {
+  let r = Math.floor(Math.random() * 256);
+  let g = Math.floor(Math.random() * 256);
+  let b = Math.floor(Math.random() * 256);
+  return `rgb(${r},${g},${b})`;
+}
+console.log(colors);
