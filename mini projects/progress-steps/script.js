b7a36d8d4f4890754162f9cffbd8c489ac1769d0
rony <touhid_rony@yahoo.com>
2021-07-26 04:40:20 +0600

    2nd project- progress bar

--- mini projects/progress-steps/script.js
+++ mini projects/progress-steps/script.js
@@ -1,49 +1,46 @@
-const progress = document.getElementById('progress')
-const prev = document.getElementById('prev')
-const next = document.getElementById('next')
-const circles = document.querySelectorAll('.circle')
+const progress = document.getElementById("progress");
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+const circles = document.querySelectorAll(".circle");
 
-let currentActive = 1
+let currentLength = 1;
 
-next.addEventListener('click', () => {
-    currentActive++
+next.addEventListener("click", () => {
+  currentLength++;
+  if (currentLength > circles.length) {
+    currentLength = circles.length;
+  }
 
-    if(currentActive > circles.length) {
-        currentActive = circles.length
-    }
+  mainFunc();
+});
 
-    update()
-})
+prev.addEventListener("click", () => {
+  currentLength--;
+  if (currentLength == 1) {
+    currentLength = 1;
+  }
+  mainFunc();
+});
 
-prev.addEventListener('click', () => {
-    currentActive--
-
-    if(currentActive < 1) {
-        currentActive = 1
-    }
-
-    update()
-})
-
-function update() {
-    circles.forEach((circle, idx) => {
-        if(idx < currentActive) {
-            circle.classList.add('active')
-        } else {
-            circle.classList.remove('active')
-        }
-    })
-
-    const actives = document.querySelectorAll('.active')
-
-    progress.style.width = (actives.length - 1) / (circles.length - 1) * 100 + '%'
-
-    if(currentActive === 1) {
-        prev.disabled = true
-    } else if(currentActive === circles.length) {
-        next.disabled = true
+function mainFunc() {
+  circles.forEach((circle, index) => {
+    if (index < currentLength) {
+      circle.classList.add("active");
     } else {
-        prev.disabled = false
-        next.disabled = false
+      circle.classList.remove("active");
     }
+  });
+  const actives = document.querySelectorAll(".active");
+
+  progress.style.width = ((actives.length - 1) / (circles.length - 1)) * 100 + "%";
+
+  if (currentLength == 1) {
+    prev.disabled = true;
+  } else if (currentLength == circles.length) {
+    prev.disabled = false;
+    next.disabled = true;
+  } else {
+    prev.disabled = false;
+    next.disabled = false;
+  }
 }
