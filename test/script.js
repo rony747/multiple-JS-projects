e81b6bd4d4393910481537fe24e690b8c13500ae
rony <touhid_rony@yahoo.com>
2021-08-17 03:58:31 +0600

    weather app

--- test/script.js
+++ test/script.js
@@ -1,19 +1,10 @@
 'use strict';
-const hex = [0, 1, 2, 3, 4, 5, 6, 'A', 'B', 'C', 'D', 'E', 'F'];
-const btn = document.getElementById('colorBtn');
-const currentColor = document.getElementById('currentColor');
 
-btn.addEventListener('click', genColor);
-
-function genColor() {
-  let hexColor = '#';
-  for (let i = 0; i < 6; i++) {
-    hexColor += hex[randNum()];
-  }
-  currentColor.innerText = hexColor;
-  document.body.style.backgroundColor = hexColor;
-  console.log(randNum);
-}
-function randNum() {
-  return Math.floor(Math.random() * hex.length);
-}
+const gerTodos = async () => {
+  const response = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await response.json();
+  return data;
+};
+gerTodos().then(data => {
+  console.log(data);
+});
